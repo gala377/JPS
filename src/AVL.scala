b3055f81e843babed420b85e71ff97a7fc7f72b9
gala377 <r.galczak@gmail.com>
2018-05-27 16:50:53 +0200

    avl scratch

--- src/AVL.scala
+++ src/AVL.scala
@@ -1,5 +1,24 @@
 package src
 
-class AVL {
+class AVL[T] {
+
+
+  case class Node (value: T, left: Option[Node], right: Option[Node]) {
+    def this(value: T) = this(value, None, None)
+  }
+
+
+  private val root: Option[Node] = None
+
+  def depth(): Int = {
+    root match {
+      case Some(node) => subtree_depth(node)
+      case None => 0
+    }
+  }
+
+  private def subtree_depth(node: Node): Int = {
+    
+  }
 
 }
